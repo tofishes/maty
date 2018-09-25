fa54286fefbaba6bf48f07bce2ecd558ef767eb2
bodhi <bodhi@lianj.com>
2018-09-25 15:21:47 +0800

    chore: update match stage

--- stages/match.js
+++ stages/match.js
@@ -1,11 +1,6 @@
-/**
- * 匹配route
- * 两种匹配方法： 1、用filter得到全部匹配； 2、for循环得到一个匹配就终止；
- * 目前按第二种方式
- */
+// 得到参数
+// https://github.com/expressjs/express/blob/master/lib/router/layer.js
 function getParam(pathInfo, paramKeys) {
-  // 得到参数
-  // https://github.com/expressjs/express/blob/master/lib/router/layer.js
   const param = {};
 
   for (let j = 1; j < pathInfo.length; j++) {
@@ -21,6 +16,11 @@ function getParam(pathInfo, paramKeys) {
   return param;
 }
 
+/**
+ * 匹配route
+ * 两种匹配方法： 1、用filter得到全部匹配； 2、for循环得到一个匹配就终止；
+ * 目前按第二种方式
+ */
 function matchRouter(pathname, routers) {
   for (let i = 0, l = routers.length, router, pathInfo; i < l; i++) {
     router = routers[i];
@@ -45,6 +45,8 @@ function matchInterceptor(req, interceptors) {
 
     const matchPath = interceptor.pathRegx.test(pathname);
     const route = interceptor.route;
+
+    // 排除静态文件，除非主动匹配
     const routeExt = ~route.indexOf('.');
     const ignoreExt = routeExt || !~pathname.indexOf('.');
 
@@ -55,10 +57,7 @@ function matchInterceptor(req, interceptors) {
 function match(ctx, next) {
   const { request, stage } = ctx;
 
-  const pathname = request.pathname;
-  const routers = stage.get('routers');
-
-  const { router, param = {} } = matchRouter(pathname, routers);
+  const { router, param = {} } = matchRouter(request.path, stage.get('routers'));
   request.interceptors = matchInterceptor(request, stage.get('interceptors'));
 
   request.param = param;
@@ -72,16 +71,17 @@ function match(ctx, next) {
   const supportRouter = router.methods.includes(method);
 
   if (!supportRouter) {
-    throw new Error(`405 ${method.toUpperCase()} Method Not Allowed`);
+    ctx.status = 405;
+    ctx.body = `${method.toUpperCase()} Method Not Allowed`;
+    return;
   }
 
   request.router = router;
   // 合并参数
   Object.assign(request.query, param);
-  // 若未使用body-parser，request.body为undefined
   Object.assign(request.body, param);
 
   return next();
 }
 
-module.exports = matchRouter;
+module.exports = match;

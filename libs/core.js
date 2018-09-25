fadadf48689a81b411bd3a3141fbda79bd02cdb9
bodhi <bodhi@lianj.com>
2018-09-25 10:56:41 +0800

    chore: modify core files

---
+++ libs/core.js
@@ -0,0 +1,99 @@
+const glob = require('glob');
+const nunjucks = require('nunjucks');
+
+const env = require('../utils/env');
+const memoryCache = require('../utils/memory-cache');
+const parseMultiName = require('../utils/parse-multi-name');
+const parseRouter = require('./parse-router');
+const Stage = require('./stage');
+
+const pwd = process.cwd();
+const defaultRouterDir = `${pwd}/routers`;
+const defaultInterceptorDir = `${pwd}/interceptors`;
+const defaultViewDir = `${pwd}/views`;
+const defaultUploadDir = `${pwd}/uploads`;
+
+const pageInfo = require('../stages/page-info');
+const initHttpRequest = require('../stages/init-http-request');
+const match = require('../stages/match');
+const handleInterceptor = require('../stages/handle-interceptor');
+const handleRouter = require('../stages/handle-router');
+const requestProxy = require('../stages/request-proxy');
+const runTask = require('../stages/run-task');
+const getViewPath = require('../stages/get-view-path');
+const render = require('../stages/render');
+
+function loadRoutes(dir) {
+  const map = {};
+  const files = glob.sync(`${dir}/**/*.js`);
+
+  files.forEach(file => {
+    const routesPart = require(file); // eslint-disable-line
+    Object.assign(map, parseMultiName(routesPart));
+  });
+
+  return map;
+}
+
+function simpleApiDataName(api) {
+  return api.substr(api.lastIndexOf('/') + 1);
+}
+
+/**
+ * 框架maty，基于koa.js用于简化前端页面直出流程的框架
+ *
+ * @param  {[type]} args 配置对象
+ * args.routerDir: 路由配置目录
+ * args.handleAPI: 预处理api地址
+ * @return {[type]}      [description]
+ */
+module.exports = (args = {}) => {
+  const {
+    routerDir = defaultRouterDir,           // 路由目录
+    interceptorDir = defaultInterceptorDir, // 拦截器目录
+    viewDir = defaultViewDir,               // 视图模板目录
+    viewExclude = ['**/include/**'],        // 排除自动渲染模板的目录，采用glob匹配规则
+    apiDataCache = memoryCache,             // 接口数据缓存方法，默认存储于内存中
+    apiDataName = simpleApiDataName,        // 接口数据名方法，默认为获取api地址最后一个/后面的单词名
+    handleAPI = url => url,                 // router.api地址预处理方法，默认返回自身
+    ajaxCache = true                        // 是否允许缓存ajax响应结果，默认允许缓存
+  } = args;
+
+  const interceptorMap = loadRoutes(interceptorDir);
+  const routerMap = loadRoutes(routerDir);
+  const routers = parseRouter(routerMap);
+  const interceptors = parseRouter(interceptorMap, interceptor => {
+    interceptor.type = 'interceptor';
+  });
+
+  const stage = new Stage();
+
+  // 存储
+  stage.set('interceptorMap', interceptorMap);
+  stage.set('interceptors', interceptors);
+  stage.set('routerMap', routerMap);
+  stage.set('routers', routers);
+  stage.set('views', viewDir);
+  stage.set('viewExclude', viewExclude);
+  // 保存接口数据缓存方法
+  stage.set('apiDataCache', apiDataCache);
+  // 保存接口数据名方法
+  stage.set('apiDataName', apiDataName);
+  // 保存接口地址处理方法
+  stage.set('handleAPI', handleAPI);
+
+  stage.set('ajaxCache', ajaxCache);
+  stage.set('uploadDir', defaultUploadDir);
+
+  // 添加默认模板引擎
+  const nunjucksEnv = nunjucks.configure(viewDir, {
+    autoescape: true,
+    noCache: env.isDev,
+    watch: env.isDev
+  });
+  stage.set('nunjucks', nunjucks);
+  stage.set('nunjucksEnv', nunjucksEnv);
+  stage.engine('njk', nunjucksEnv.render.bind(nunjucksEnv));
+
+  return stage;
+};
---
+++ libs/stage.js
@@ -0,0 +1,120 @@
+// class Stage
+const qs = require('qs');
+const minimatch = require('minimatch');
+const bodyParser = require('koa-body');
+
+const urlInfo = require('../utils/url-info');
+const concat = Array.prototype.concat;
+const defaultLimit = 1000 * 1024 * 1024; // 1000M
+
+class Stage {
+  constructor() {
+    const filterMap = {};
+
+    this.stages = ['request', 'response'];
+    this.stages.map(name => {
+      filterMap[name] = [];
+    });
+
+    this.filters = [];
+    this.filterMap = filterMap;
+    this.props = {};
+    this.engines = {};
+  }
+
+  merge() {
+    // 合并所有的filters
+    this.stages.map(name => {
+      this.filters = this.filters.concat(this.filterMap[name]);
+    });
+  }
+  // 添加filter
+  on(stageName, action) {
+    const filters = this.filterMap[stageName]; // this.filterMap[stagesName];
+
+    if (!filters) {
+      throw new Error(`Stage ${name} is not supported!`);
+    }
+
+    action.isFilter = true;
+    action.role = `filter-${stageName}`;
+
+    filters.push(action);
+
+    return this;
+  }
+
+  set(name, value) {
+    this.props[name] = value;
+    return this;
+  }
+
+  get(name) {
+    return this.props[name];
+  }
+
+  engine(extName, render) {
+    this.engines[`.${extName}`] = render;
+    return this;
+  }
+
+  convert(stage, mountPath) {
+    return (ctx, next) => {
+      if (!ctx.isEnd) {
+        return ctx.body;
+      }
+
+      if (!mountPath) {
+        return stage(ctx, next);
+      }
+
+      if (typeof ctx.isMatchMount !== 'boolean') {
+        ctx.isMatchMount = minimatch(ctx.path, mountPath);
+      }
+
+      if (ctx.isMatchMount) {
+        return stage(ctx, next);
+      }
+
+      return next();
+    }
+  }
+
+  mount(app, path) {
+    this.merge();
+
+    this.filters.map(filter => {
+      app.use(this.convert(filter, path));
+    })
+  }
+
+  listen(...args) {
+    const app = new Koa();
+
+    app.use(bodyParser({
+      jsonLimit: defaultLimit,
+      formLimit: defaultLimit,
+      textLimit: defaultLimit,
+      multipart: true,
+      formidable: {
+        keepExtensions: true,
+        maxFieldsSize: defaultLimit,
+        uploadDir: this.get('uploadDir')
+      }
+    }));
+
+    // 扩展context
+    Object.defineProperty(app.context, 'isEnd', {
+      get() {
+        return this.body !== undefined;
+      }
+    });
+
+    app.context.stage = this;
+    this.mount(app);
+
+    app.listen.apply(app, args);
+  }
+}
+
+module.exports = Stage;

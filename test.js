cfbb75d2d31bb2bff87222b24fe8018c66c57877
bodhi <bodhi@lianj.com>
2018-09-29 17:01:02 +0800

    chore: ignore this file, this is some test

--- test.js
+++ test.js
@@ -1,20 +1,35 @@
 const koa = require('koa');
+const LRU = require("lru-cache")
 
 const app = new koa();
 
-Reflect.defineProperty(app.context, 'isEnd', {
-  get() {
-    console.log(this.body, '**')
-    return this.body !== undefined;
-  }
-});
+const cache = LRU();
 
-app.use((ctx, next) => {
-  console.log(ctx.isEnd, '--', ctx.path);
+cache.set('a', 'a', 2000);
+cache.set('b', 'b');
+cache.set('c', 'c');
 
-  ctx.body = 'ok';
+console.log(cache.get('a'));
+console.log(cache.get('b'));
+console.log(cache.get('c'));
 
-  console.log(ctx.isEnd, '==');
-});
+setTimeout(() => {
+  console.log(cache.get('a'));
+}, 2500)
 
-app.listen(9999)
+// Reflect.defineProperty(app.context, 'isEnd', {
+//   get() {
+//     console.log(this.body, '**')
+//     return this.body !== undefined;
+//   }
+// });
+
+// app.use((ctx, next) => {
+//   console.log(ctx.isEnd, '--', ctx.path);
+
+//   ctx.body = 'ok';
+
+//   console.log(ctx.isEnd, '==');
+// });
+
+// app.listen(9999)
